b968652db4bdd028bc1b91ce8e48dec6892f1307
ericcui <eric.cui@logisticsplus.com>
2024-08-03 20:40:04 -0400

    Event posting endpoint completed

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -80,7 +80,7 @@ export default function Home(){
   const createEventSaveBody = () => {
     return {
       "user": user,
-      "events": events
+      "events": JSON.stringify(events)
     }
   }
 
